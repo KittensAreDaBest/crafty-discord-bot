a41204189280ac34f430230b16262f9d1a8bf71a
KittensAreDaBest <60989180+KittensAreDaBest@users.noreply.github.com>
2020-07-31 10:34:51 +1000

    Cleaned up some code
    Enabled Logging for admin commands
    Allowed user to easily turn off or on the !alert command
    Rewrote some code

--- discordconnect.py
+++ discordconnect.py
@@ -1,26 +1,33 @@
-import discord
-from crafty_client import CraftyWeb
 import re
-from discord.ext import commands
 import string
 
-URL = "https://" # WebServer
-API_TOKEN = "" # Crafty API Token
-BOT_TOKEN = "" # Discord Bot Token
-STARTUP_MESSAGE_ID = "" # Discord Startup Channel ID
-craftyperms = "" # Role bot searches for and if have role then can execute admin commands
+import discord
+from discord.ext import commands
 
-STARTUP_MESSAGE_ID_INT = int(STARTUP_MESSAGE_ID) # Converts STR to INT
+from crafty_client import CraftyWeb
 
-cweb = CraftyWeb(URL, API_TOKEN)
+URL = ""  # WebServer
+API_TOKEN = ""  # Crafty API Token
+BOT_TOKEN = ""  # Discord Bot Token
+logging_channel = ""  # Discord Startup Channel ID
+craftyperms = ""  # Role bot searches for and if have role then can execute admin commands
+
+# Enable Alert command, a yes or no answer THIS REQUIRES YOU TO HAVE A PROXY SERVER
+enablealert = "no"
+
+# What is the proxy server id?
+proxyid = "0"
 
 client = discord.Client()
+
+cweb = CraftyWeb(URL, API_TOKEN)
 
 client = commands.Bot(command_prefix='!')
 client.remove_command('help')
 
+
 def convertTuple(tup):
-    str =  " ".join(tup)
+    str = " ".join(tup)
     return str
 
 
@@ -38,9 +45,8 @@ def listToString(s):
 
 @client.event
 async def on_ready():
-    sendstart = client.get_channel(STARTUP_MESSAGE_ID_INT) # channel to send start message
-    author = await client.fetch_user(259875936852246528) # gets authors id
-
+    logs = client.get_channel(int(logging_channel))  # channel to send start message
+    author = await client.fetch_user(259875936852246528)
     print("Bot is ready!")
     print("Logged in as: " + client.user.name)
     print("Bot ID: " + str(client.user.id))
@@ -55,11 +61,12 @@ async def on_ready():
         print("Connected to server: {}".format(guild))
     authorstring = str(author)
     embedstartup.set_footer(text="Created by " + authorstring)
-    await sendstart.send(embed=embedstartup) # sends startup meessage
+    await logs.send(embed=embedstartup)  # sends startup meessage
 
 
 @client.command()
 async def send(ctx, arg, *arg4):
+    logs = client.get_channel(int(logging_channel))  # channel to send log message
     strsend = convertTuple(arg4)
     role = discord.utils.get(ctx.guild.roles, name=craftyperms)
     if role in ctx.author.roles:
@@ -69,78 +76,97 @@ async def send(ctx, arg, *arg4):
         sendembed.add_field(name="Action Sucessful?", value=cweb.run_command(arg, strsend), inline=True)
         sendembed.add_field(name="Requested by", value=ctx.message.author.mention, inline=True)
         await ctx.send(embed=sendembed)
+        await logs.send(embed=sendembed)
     else:
         await ctx.channel.send("You do not have permission to run this command.")
 
-# @client.command() # this command is commented out because of no proxy server on ID 5
-# async def alert(ctx, *arg5):
-#     str = convertTuple(arg5)
-#     argexe = "5"
-#     alertas = "alert"
-#     output = (alertas + " " + str)
-#     role = discord.utils.get(ctx.guild.roles, name=craftyperms)
-#     if role in ctx.author.roles:
-#         sendembedalert = discord.Embed(title="Crafty Alert Execution", color=0x31c4b3)
-#         sendembedalert.add_field(name="Command", value=str, inline=True)
-#         sendembedalert.add_field(name="Action Sucessful?", value=cweb.run_command(argexe, output), inline=True)
-#         sendembedalert.add_field(name="Requested by", value=ctx.message.author.mention, inline=True)
-#         await ctx.send(embed=sendembedalert)
-#     else:
-#         await ctx.channel.send("You do not have permission to run this command.")
+
+@client.command()
+async def alert(ctx, *arg5):
+    logs = client.get_channel(int(logging_channel))  # channel to send log message
+    str = convertTuple(arg5)
+    output = ("alert " + str)
+    role = discord.utils.get(ctx.guild.roles, name=craftyperms)
+    if enablealert == "yes":
+        if role in ctx.author.roles:
+            sendembedalert = discord.Embed(title="Crafty Alert Execution", color=0x31c4b3)
+            sendembedalert.add_field(name="Command", value=str, inline=True)
+            sendembedalert.add_field(name="Action Sucessful?", value=cweb.run_command(proxyid, output), inline=True)
+            sendembedalert.add_field(name="Requested by", value=ctx.message.author.mention, inline=True)
+            await ctx.send(embed=sendembedalert)
+            await logs.send(embed=sendembedalert)
+        else:
+            await ctx.channel.send("You do not have permission to run this command.")
+    else:
+        await ctx.channel.send("The Alert Command has been disabled / not setup " + ctx.message.author.mention)
+
 
 @client.command()
 async def backup(ctx, args):
+    logs = client.get_channel(int(logging_channel))  # channel to send log message
     if "all" in args:
         embedbackup = discord.Embed(title="Crafty Backup Execution", color=0x31c4b3)
+        # for the backup all command list your servers like the example provided below
         embedbackup.add_field(name="Backup Server ID 1 Sucessful?", value=cweb.backup_server(1), inline=True)
-        embedbackup.add_field(name="Backup Server ID 2 Sucessful?", value=cweb.backup_server(2), inline=True)
-        embedbackup.add_field(name="Backup Server ID 3 Sucessful?", value=cweb.backup_server(3), inline=True)
-        embedbackup.add_field(name="Backup Server ID 4 Sucessful?", value=cweb.backup_server(4), inline=True)
+        # -------------------------------------------------------------------------
         embedbackup.add_field(name="Requested by", value=ctx.message.author.mention, inline=True)
         await ctx.send(embed=embedbackup)
+        await logs.send(embed=embedbackup)
 
     else:
         embedbackup2 = discord.Embed(title="Crafty Backup Execution", color=0x31c4b3)
-        embedbackup2.add_field(name="Sending Backup Request to ID" + args + " Sucessful?", value=cweb.backup_server(args), inline=True)
+        embedbackup2.add_field(name="Sending Backup Request to ID" + args + " Sucessful?",
+                               value=cweb.backup_server(args), inline=True)
         embedbackup2.add_field(name="Requested by", value=ctx.message.author.mention, inline=True)
         await ctx.send(embed=embedbackup2)
+        await logs.send(embed=embedbackup2)
+
 
 @client.command()
 async def restart(ctx, argid):
     role = discord.utils.get(ctx.guild.roles, name=craftyperms)
     if role in ctx.author.roles:
+        logs = client.get_channel(int(logging_channel))  # channel to send log message
         embedrestart = discord.Embed(title="Crafty Restart Execution", color=0x31c4b3)
         embedrestart.add_field(name="Restart Server ", value="ID " + argid, inline=True)
         embedrestart.add_field(name="Action Sucessful?", value=cweb.restart_server(argid), inline=True)
         embedrestart.add_field(name="Requested by", value=ctx.message.author.mention, inline=True)
         await ctx.send(embed=embedrestart)
+        await logs.send(embed=embedrestart)
     else:
         await ctx.channel.send("You do not have permission to run this command.")
+
 
 @client.command()
 async def stop(ctx, argid):
     role = discord.utils.get(ctx.guild.roles, name=craftyperms)
     if role in ctx.author.roles:
+        logs = client.get_channel(int(logging_channel))  # channel to send log message
         embedstop = discord.Embed(title="Crafty Stop Execution", color=0x31c4b3)
         embedstop.add_field(name="Stop Server ", value="ID " + argid, inline=True)
         embedstop.add_field(name="Action Sucessful?", value=cweb.stop_server(argid), inline=True)
         embedstop.add_field(name="Requested by", value=ctx.message.author.mention, inline=True)
         await ctx.send(embed=embedstop)
+        await logs.send(embed=embedstop)
 
     else:
         await ctx.channel.send("You do not have permission to run this command.")
+
 
 @client.command()
 async def start(ctx, argid):
     role = discord.utils.get(ctx.guild.roles, name=craftyperms)
     if role in ctx.author.roles:
+        logs = client.get_channel(int(logging_channel))  # channel to send log message
         embedstart = discord.Embed(title="Crafty Start Execution", color=0x31c4b3)
         embedstart.add_field(name="Start Server ", value="ID " + argid, inline=True)
         embedstart.add_field(name="Action Sucessful?", value=cweb.start_server(argid), inline=True)
         embedstart.add_field(name="Requested by", value=ctx.message.author.mention, inline=True)
         await ctx.send(embed=embedstart)
+        await logs.send(embed=embedstart)
     else:
         await ctx.channel.send("You do not have permission to run this command.")
+
 
 @client.command()
 async def hostinfo(message):
@@ -149,7 +175,7 @@ async def hostinfo(message):
     else:
         embedhoststatus = discord.Embed(title="Crafty Host Info", color=0x31c4b3)
         hostdata = cweb.get_host_stats_pc()
-        hostcpu = re.findall(r'cpu_usage(.*?),',str(hostdata))
+        hostcpu = re.findall(r'cpu_usage(.*?),', str(hostdata))
         punctuationwittdot = "#$%&'()*+,-/:;<=>?@[\]^_`{|}~"
         datastrip = str.maketrans("", "", punctuationwittdot)
 
@@ -158,9 +184,9 @@ async def hostinfo(message):
 
         embedhoststatus.add_field(name="CPU", value="CPU Usage" + cpuassembled + "%", inline=False)
 
-        hostmem = re.findall(r'mem_percent(.*?),',str(hostdata))
-        hostmemused = re.findall(r'mem_usage(.*?),',str(hostdata))
-        hostmemall = re.findall(r'mem_total(.*?),',str(hostdata))
+        hostmem = re.findall(r'mem_percent(.*?),', str(hostdata))
+        hostmemused = re.findall(r'mem_usage(.*?),', str(hostdata))
+        hostmemall = re.findall(r'mem_total(.*?),', str(hostdata))
 
         memstripped = [w.translate(datastrip) for w in hostmem]
         memassembled = " ".join(memstripped)
@@ -171,11 +197,13 @@ async def hostinfo(message):
         memallstripped = [w.translate(datastrip) for w in hostmemall]
         memallassembled = " ".join(memallstripped)
 
-        embedhoststatus.add_field(name="Memory Used", value="Memory Usage" + memassembled + "%" + memusedassembled + " Used out of" + memallassembled, inline=False)
+        embedhoststatus.add_field(name="Memory Used",
+                                  value="Memory Usage" + memassembled + "%" + memusedassembled + " Used out of" + memallassembled,
+                                  inline=False)
 
-        hostdisk = re.findall(r'disk_percent(.*?),',str(hostdata))
-        hostdiskused = re.findall(r'disk_usage(.*?),',str(hostdata))
-        hostdisktotal = re.findall(r'disk_total(.*?)}',str(hostdata))
+        hostdisk = re.findall(r'disk_percent(.*?),', str(hostdata))
+        hostdiskused = re.findall(r'disk_usage(.*?),', str(hostdata))
+        hostdisktotal = re.findall(r'disk_total(.*?)}', str(hostdata))
 
         diskstripped = [w.translate(datastrip) for w in hostdisk]
         diskassembled = " ".join(diskstripped)
@@ -186,14 +214,18 @@ async def hostinfo(message):
         disktotalstripped = [w.translate(datastrip) for w in hostdisktotal]
         disktotalassembled = " ".join(disktotalstripped)
 
-        embedhoststatus.add_field(name="Disk Used", value="Disk Usage" + diskassembled + "%" + diskusedassembled + " Used out of" + disktotalassembled, inline=False)
+        embedhoststatus.add_field(name="Disk Used",
+                                  value="Disk Usage" + diskassembled + "%" + diskusedassembled + " Used out of" + disktotalassembled,
+                                  inline=False)
 
         embedhoststatus.add_field(name="Requested by", value=message.message.author.mention, inline=True)
         await message.channel.send(embed=embedhoststatus)
 
+
 @client.command()
 async def ping(message):
     await message.channel.send('Pong! {0}'.format(round(client.latency, 1)) + " Secounds")
+
 
 @client.command()
 async def help(ctx):
@@ -213,36 +245,39 @@ async def help(ctx):
     embed.set_footer(text="Created by CatsAreDaBest#3054")
     await ctx.send(embed=embed)
 
+
 @client.command()
 async def serverinfo(message):
-        embedstatus = discord.Embed(title="Crafty Server Info", description="Lists all of the Servers and their Current Status", color=0x31c4b3)
-        data = cweb.list_mc_servers()
-        datastrip = str.maketrans("", "", string.punctuation)
+    embedstatus = discord.Embed(title="Crafty Server Info",
+                                description="Lists all of the Servers and their Current Status", color=0x31c4b3)
 
-        remove = string.punctuation
-        remove = remove.replace(".", "")
-        remove = remove.replace(":", "")
-        pattern = r"[{}]".format(remove)
-        datastripnew = str.maketrans("", "", pattern)
+    # this is to list mc servers and their current status
+    data = cweb.list_mc_servers()
 
-        moredata = cweb.get_host_stats_server()
-        datavalues = ','.join(map(str, moredata))
-        print(string.punctuation)
-        datastripped = [w.translate(datastripnew) for w in datavalues]
-        datavaluesassembled = ''.join(map(str, datastripped))
-        print(datavaluesassembled)
+    # variable for removing punctuation for the status
+    datastrip = str.maketrans("", "", string.punctuation)
 
-        srvrunning = "Server running True"
-        srvstopped = "Server running False crashed False"
-        srvcrashed = "Server running False crashed True"
+    # making the variable that has all the punctuation except for : and ,
+    remove = string.punctuation
+    remove = remove.replace(".", "")
+    remove = remove.replace(":", "")
+    datastripnew = str.maketrans("", "", r"[{}]".format(remove))
 
+    # grabs the status of mc servers and formats them by removing punctuation except for : and ,
+    datavaluesassembled = ''.join(
+        map(str, [w.translate(datastripnew) for w in ','.join(map(str, cweb.get_host_stats_server()))]))
 
+    srvrunning = "Server running True"  # this is just defining variables
+    srvstopped = "Server running False crashed False"  # this is just defining variables
+    srvcrashed = "Server running False crashed True"  # this is just defining variables
 
+    # -------------------------------------------------------------------------
+    
+    # PLEASE LOOK AT FURTHEREXAMPLES.py OR READ THE INSTRUCTIONS FIRST
+    # -------------------------------------------------------------------------
 
-        embedstatus.add_field(name="Requested by", value=message.message.author.mention, inline=True)
-
-        await message.channel.send(embed=embedstatus)
-
+    # sends them all out
+    await message.channel.send(embed=embedstatus)
 
 
 client.run(BOT_TOKEN)
